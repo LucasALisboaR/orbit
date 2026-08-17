abe7799482435482945ea849a5c3147ac9ed6245
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2026-08-17 10:59:51 -0300

    refactor: remove app.html and app.scss, update app component to use inline template and styles

--- front-end-orbit/src/app/app.ts
+++ front-end-orbit/src/app/app.ts
@@ -4,9 +4,8 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  template: `<router-outlet></router-outlet>`,
+  styles: [``]
 })
 export class App {
-  protected readonly title = signal('front-end-orbit');
 }
