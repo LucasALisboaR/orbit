e3e6ad5ef7d0c42748d8bf6bb874a1a283c2b04d
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2026-08-17 17:02:28 -0300

    fix: correct route path for new user component in features.routes.ts

--- front-end-orbit/src/app/features/features.routes.ts
+++ front-end-orbit/src/app/features/features.routes.ts
@@ -3,6 +3,6 @@ import { Routes } from '@angular/router';
 export const featuresRoutes: Routes = [
   { path: 'login', loadComponent: ()=> import('./login/login').then(m => m.Login)},
   { path: 'forget-password', loadComponent: ()=> import('./forget-password/forget-password').then(m => m.ForgetPassword)},
-  { path: 'forget-password', loadComponent: ()=> import('./new-user/new-user').then(m => m.NewUser)},
+  { path: 'new-user', loadComponent: ()=> import('./new-user/new-user').then(m => m.NewUser)},
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
