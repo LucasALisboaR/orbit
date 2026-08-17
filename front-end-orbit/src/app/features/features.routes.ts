fdc71f2766591981815f32e7bbf738c3b9a103bb
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2026-08-17 16:24:14 -0300

    feat: add route for password recovery page

--- front-end-orbit/src/app/features/features.routes.ts
+++ front-end-orbit/src/app/features/features.routes.ts
@@ -2,5 +2,6 @@ import { Routes } from '@angular/router';
 
 export const featuresRoutes: Routes = [
   { path: 'login', loadComponent: ()=> import('./login/login').then(m => m.Login)},
+  { path: 'forget-password', loadComponent: ()=> import('./forget-password/forget-password').then(m => m.ForgetPassword)},
   { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
