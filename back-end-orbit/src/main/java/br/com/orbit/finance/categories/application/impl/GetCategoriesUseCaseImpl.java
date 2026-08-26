d40127048ba27aeed2ad5595815e52fd2618ebf9
Lucas A. Lisboa <69944617+LucasALisboaR@users.noreply.github.com>
2026-08-26 15:16:34 -0300

    refactor: remove unused imports in GetCategoriesUseCaseImpl
    - Eliminated unnecessary imports to streamline the code and improve readability in the GetCategoriesUseCaseImpl class.

--- back-end-orbit/src/main/java/br/com/orbit/finance/categories/application/impl/GetCategoriesUseCaseImpl.java
+++ back-end-orbit/src/main/java/br/com/orbit/finance/categories/application/impl/GetCategoriesUseCaseImpl.java
@@ -1,14 +1,12 @@
 package br.com.orbit.finance.categories.application.impl;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.orbit.finance.categories.application.dto.CategoriesPresenter;
-import br.com.orbit.finance.categories.application.usecase.GetCategoriesAvailableToUserUseCase;
 import br.com.orbit.finance.categories.application.usecase.GetCategoriesUseCase;
 import br.com.orbit.finance.categories.domain.Categories;
 import br.com.orbit.finance.categories.domain.CategoriesRepository;
